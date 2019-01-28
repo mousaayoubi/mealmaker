d21608213fa4cbd5c11c38a0b27bbda7dfc5d7e9
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-01-28 19:20:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -29,6 +29,36 @@ const menu = {
       price: dishPrice
     };
     return this._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourse(courseName) {
+    const dishes = this._courses[courseName];
+    const randomIndex = Math.floor(Math.random() * dishes.length);
+    return dishes[randomIndex];
+  },
+  generateRandomMeal() {
+    const appetizer = this.getRandomDishFromCourse("appetizers");
+    const main = this.getRandomDishFromCourse("mains");
+    const dessert = this.getRandomDishFromCourse("desserts");
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    return console.log(
+      `The name of the appetizer dish is ${
+        appetizer.name
+      }. The name of the main dish is ${
+        main.name
+      }. The name of the dessert dish is ${
+        dessert.name
+      }. The total price is $${totalPrice}.`
+    );
   }
 };
-console.log(menu);
+menu.addDishToCourse("appetizers", "green salad", 5);
+menu.addDishToCourse("appetizers", "hot soup", 3);
+menu.addDishToCourse("appetizers", "bread", 2);
+menu.addDishToCourse("mains", "grilled salmon", 10);
+menu.addDishToCourse("mains", "grilled steak", 8);
+menu.addDishToCourse("mains", "grilled chicken", 7);
+menu.addDishToCourse("desserts", "strawberry cake", 7);
+menu.addDishToCourse("desserts", "ice cream", 5);
+menu.addDishToCourse("desserts", "chocolates", 3);
+
+menu.generateRandomMeal();
